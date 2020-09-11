51b627830b3bf5d342976a5d54cf52f1eb59036a
Yoreni <yoreni@icloud.com>
2020-09-11 08:19:00 +0100

    a

--- myFirstBot.py
+++ myFirstBot.py
@@ -1,13 +1,10 @@
-import os
 import discord
-from dotenv import load_dotenv
-from discord.ext import commands
 import aiohttp
 import urllib.request
 import json
 
-#load_dotenv()
-#theToken = os.getenv('DISCORD_TOKEN')
+#contents = urllib.request.urlopen("https://scratch.mit.edu/users/yoreni/projects/").read()
+#print(contents)
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -26,18 +23,51 @@ class MyClient(discord.Client):
 					
                     try:
                         contents = json.loads(urllib.request.urlopen("https://api.scratch.mit.edu/projects/" + projectid).read())
-                        text = 'Author: ' + str(contents["author"]["username"])
-                        text = text + "\nTitle: " + str(contents["title"])
-                        text = text + "\nViews: " + str(contents["stats"]["views"])
-                        text = text + "\nLoves: " + str(contents["stats"]["loves"])
-                        text = text + "\nFavorites: " + str(contents["stats"]["favorites"])
-                        text = text + "\nRemixes: " + str(contents["stats"]["remixes"])
-                        text = text + "\nComments: " + str(contents["stats"]["comments"])
-                        text = text + "\nLink: https://scratch.mit.edu/projects/" + str(projectid)
+                        text = "**" + str(contents["title"]) + ' by ' + str(contents["author"]["username"]) + "**"
+                        text = text + "\n> :link: https://scratch.mit.edu/projects/" + str(projectid)
+                        text = text + "\n:eye:" + str(contents["stats"]["views"])
+                        text = text + "  :heart:" + str(contents["stats"]["loves"])
+                        text = text + "  :star: " + str(contents["stats"]["favorites"])
+                        text = text + "  :cyclone: " + str(contents["stats"]["remixes"])
+                        text = text + "  :speech_left: " + str(contents["stats"]["comments"])
+                        
+                        if (len(str(contents["description"])) > 0):
+                            desc = str(contents["description"])
+                            if(len(desc) > 300):
+                                desc = desc[:300] + "..."
+                            desc = desc.replace("```","\```",100)
+                            desc = desc.replace("`","\`",100)
+                            desc = desc.replace("**","\**",100)
+                            text = text + "\nNotes and Credits: ```" + desc + "```"
+                        if (len(str(contents["instructions"])) > 0):
+                            desc = str(contents["instructions"])
+                            if(len(desc) > 300):
+                                desc = desc[:300] + "..."
+                            desc = desc.replace("```","\```",100)
+                            desc = desc.replace("`","\`",100)
+                            desc = desc.replace("**","\**",100)
+                            text = text + "\nInstructions: ```" + desc + "```"
                         await message.channel.send(text)
                     except urllib.error.HTTPError:
                         await message.channel.send("An error happened. maybe this project hasnt been shared yet or maybe it doesnt exist.")
-				
+            if(commandArgs[1] == "user"):
+                name = commandArgs[2]
+                try:
+                    contents = json.loads(urllib.request.urlopen("https://api.scratch.mit.edu/users/" + name).read())
+                    #followers = int(str(urllib.request.urlopen("https://scratch.mit.edu/users/" + name + "/followers/").read()).split("Followers (")[1].split(")")[0])
+                    #projects = int(str(urllib.request.urlopen("https://scratch.mit.edu/users/" + name + "/projects/").read()).split("Shared Projects (")[1].split(")")[0])
+                    
+                    text = 'Username: ' + str(contents["username"])
+                    text = text + "\nLocation: " + str(contents["profile"]["country"])
+                    text = text + "\nJoin Date: " + str(contents["history"]["joined"][:10])
+                    #text = text + "\nFollowers: " + str(followers)
+                    #text = text + "\nShared Projects: " + str(projects)
+                    text = text + "\nBio: " + str(contents["profile"]["bio"])
+                    text = text + "\nWhat I'm Working On: " + str(contents["profile"]["status"])
+                    text = text + "\nLink: https://scratch.mit.edu/users/" + str(name)
+                    await message.channel.send(text)
+                except urllib.error.HTTPError:
+                    await message.channel.send("An error happened. this user doesnt exist.")
 				
             #print('Message from {0.author}: {0.content}'.format(message))
 
@@ -46,44 +76,3 @@ client = MyClient(connector=aiohttp.TCPConnector(ssl=False))
 f = open('secret.txt',"r")
 data = json.load(f)
 client.run(data["token"])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
